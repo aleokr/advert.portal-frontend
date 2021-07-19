6ba4b32f563b0b3212522cff0b7311c2b22d4bfc
aleokr <aleksandra.okrutny1@gmial.com>
2021-07-19 19:21:22 +0200

    Initial commit

---
+++ src/test/start.spec.tsx
@@ -0,0 +1,23 @@
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+    expect(filterByTerm(input, "link")).toEqual(output);
+
+  });
+});
+function filterByTerm(inputArr, searchTerm) {
+  return inputArr.filter(function(arrayElement) {
+    return arrayElement.url.match(searchTerm);
+  });
+}
